ed11db33240f80a57d812f10a39c493c8e7a94c0
Matt Palmer <mpalmer@hezmatt.org>
2023-06-01 09:08:00 +1000

    Nothing is too trivial to test

--- src/utils.rs
+++ src/utils.rs
@@ -10,8 +10,48 @@ use url::Url;
 /// handling, we'll just add a slash ourselves if necessary.
 #[must_use]
 pub fn fix_url(mut url: Url) -> Url {
-	if url.path().ends_with('/') {
+	if !url.path().ends_with('/') {
 		url.set_path(&format!("{}/", url.path()));
 	};
 	url
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn fixed_url_mangles_unslashed_base_domain() {
+		assert_eq!("https://example.com/", fix_url(Url::parse("https://example.com").unwrap()).to_string());
+	}
+
+	#[test]
+	fn fixed_url_leaves_slashed_base_domain() {
+		assert_eq!("https://example.com/", fix_url(Url::parse("https://example.com/").unwrap()).to_string());
+	}
+
+	#[test]
+	fn fixed_url_joins_to_base_domain_correctly() {
+		assert_eq!("https://example.com/foo/bar/baz", fix_url(Url::parse("https://example.com").unwrap()).join("foo/bar/baz").unwrap().to_string());
+	}
+
+	#[test]
+	fn fixed_url_joins_to_slashed_base_domain_correctly() {
+		assert_eq!("https://example.com/foo/bar/baz", fix_url(Url::parse("https://example.com/").unwrap()).join("foo/bar/baz").unwrap().to_string());
+	}
+
+	#[test]
+	fn fixed_url_joins_to_pathed_domain_correctly() {
+		assert_eq!("https://example.com/foo/bar/baz", fix_url(Url::parse("https://example.com/foo/bar").unwrap()).join("baz").unwrap().to_string());
+	}
+
+	#[test]
+	fn fixed_url_joins_to_slashed_pathed_domain_correctly() {
+		assert_eq!("https://example.com/foo/bar/baz", fix_url(Url::parse("https://example.com/foo/bar/").unwrap()).join("baz").unwrap().to_string());
+	}
+
+	#[test]
+	fn crate_example() {
+		assert_eq!("https://example.net/a/b/c.png", fix_url(Url::parse("https://example.net/a/b/").unwrap()).join("c.png").unwrap().to_string());
+	}
 }
